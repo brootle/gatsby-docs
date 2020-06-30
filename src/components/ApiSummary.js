68e96694c8468bed24fd9e0772824570a1a7e225
Alexander Cherednichenko <luganskukraine@gmail.com>
2020-06-30 16:50:06 +0300

    all blocks for api summary added to index of documentation

--- src/components/ApiSummary.js
+++ src/components/ApiSummary.js
@@ -7,49 +7,80 @@ export default function ApiSummary({ title }) {
 
     const apiReferenceIndexPage = useStaticQuery(graphql`
         query {
-            allMdx(filter: {frontmatter: {dir: {eq: "API Reference"}}}) {
+            allMdx(filter: {fields: {slug: {regex: "/api-reference/"}}}, limit: 4, sort: {fields: frontmatter___weight, order: DESC}) {
                 edges {
-                    node {
-                        id
-                        frontmatter {
-                            post_title
-                            dir
-                            weight
-                        }
-                        fields {
-                            slug
-                        }
-                        excerpt(pruneLength: 180)
+                  node {
+                    id
+                    frontmatter {
+                      post_title
+                      dir
+                      weight
                     }
+                    fields {
+                      slug
+                    }
+                    excerpt(pruneLength: 180)
+                  }
                 }
-            }
+              }
         }
     `)    
 
+    // get index page that matches /api-reference/ slug exactly
+    const index = apiReferenceIndexPage.allMdx.edges.filter( ({node}) => node.fields.slug === "/api-reference/" )[0];
+
+    // get other nodes from api reference section
     const indexPage = {
-        title: apiReferenceIndexPage.allMdx.edges[0].node.frontmatter.post_title,
-        summary: apiReferenceIndexPage.allMdx.edges[0].node.excerpt
+        title: index.node.frontmatter.post_title,
+        summary: index.node.excerpt
     }
 
+    const apiNodes = apiReferenceIndexPage.allMdx.edges.filter( ({node}) => node.fields.slug !== "/api-reference/" );
 
-    console.log("apiReferenceIndexPage: ", apiReferenceIndexPage.allMdx.edges[0].node)
+    const apiPosts = apiNodes.map( ({ node }) => {
+        let dir = node.fields.slug.slice(1, -1).split('/')[1]
+        let excerpt = node.excerpt.slice(0, 40) + '...'
+        return {
+            title: node.frontmatter.post_title, 
+            dir: dir,
+            summary: excerpt,
+            url: node.fields.slug
+        }
+    })
+
+    const apiPostsComponent = apiPosts.map( (post, index) => 
+        <Link to={post.url} key={index} className={apiSummaryStyles.postBlock} >
+            <div className={apiSummaryStyles.postBlock__image}>
+                <img src={withPrefix(`/images/docs/api-reference/${post.dir}.svg`)} alt={post.title} />
+            </div>
+            <div>
+                <h4>{post.title}</h4>
+                <p>{post.summary}</p>
+            </div>
+        </Link>
+    )
 
     return(
         <section>
             <h2>{title}</h2>
-            <div className={apiSummaryStyles.summaryLinkBlock} >
-                <div className={apiSummaryStyles.summaryCard} >
-                    <div className={apiSummaryStyles.icon}>
-                        <img src={withPrefix(`/images/docs/api-reference/index.svg`)} alt="API Reference" />
-                    </div>        
-                    <div>
-                        <h4>{indexPage.title}</h4>
-                        <p>{indexPage.summary}</p>
+            <Link to={`/api-reference/`}>
+                <div className={apiSummaryStyles.summaryLinkBlock} >
+                    <div className={apiSummaryStyles.summaryCard} >
+                        <div className={apiSummaryStyles.icon}>
+                            <img src={withPrefix(`/images/docs/api-reference/index.svg`)} alt="API Reference" />
+                        </div>        
+                        <div>
+                            <h4>{indexPage.title}</h4>
+                            <p>{indexPage.summary}</p>
+                        </div>
+                    </div>
+                    <div className={apiSummaryStyles.featuredImage}>
+                        <img src={withPrefix(`/images/docs/api-reference/index__featured.png`)} alt="Response Example" />
                     </div>
                 </div>
-                <div className={apiSummaryStyles.featuredImage}>
-                    <img src={withPrefix(`/images/docs/api-reference/index__featured.png`)} alt="Response Example" />
-                </div>
+            </Link>
+            <div className={apiSummaryStyles.postsList}>
+                {apiPostsComponent}
             </div>
         </section>
     )
